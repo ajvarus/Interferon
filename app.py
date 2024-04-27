8e11277e9f125db9f5c44492f8b7e16ea5d9ef28
Arjun Sen <arjunsen1997@gmail.com>
2024-04-27 21:51:47 +0100

    Revert "Added a database connection and a new api route."
    This reverts commit 72f654b313d8a7cb55a1341c6ed709984f7d9bfd.

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 
-from flask import Flask, render_template, jsonify
-from database import load_encryption_types
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
@@ -8,10 +7,6 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return render_template("home.html")
-
-@app.route("/api/encryption_types")
-def show_encryption_types():
-    return jsonify(load_encryption_types())
 
 
 if __name__ == "__main__":
