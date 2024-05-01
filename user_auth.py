c3d49b9b1f3a89653f84dab58cd5b577ae2924bb
Arjun Sen <arjunsen1997@gmail.com>
2024-05-01 02:54:21 +0100

    Implemented user registration and cleaned up code.

--- user_auth.py
+++ user_auth.py
@@ -3,12 +3,16 @@ from firebase_admin import credentials
 import dotenv
 import os
 
-dotenv.load_dotenv()
+
 
 def init_auth():
+
+    dotenv.load_dotenv()
+
+
     try:
         cred = credentials.Certificate(os.getenv("FIREBASE_KEY_PATH"))
-        app = firebase_admin.initialize_app(cred)
+        firebase_admin.initialize_app(cred)
     except Exception as e:
         print(f"Error: {str(e)}")
 
