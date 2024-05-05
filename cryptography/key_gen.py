8d20e19d55e26a2129051642f100253c21bffac3
Arjun Sen <arjunsen1997@gmail.com>
2024-05-05 19:56:27 +0100

    Added bytes to string and string to bytes conversion for keys.

--- cryptography/key_gen.py
+++ cryptography/key_gen.py
@@ -1,6 +1,7 @@
 from Crypto.Protocol.KDF import PBKDF2
 from Crypto.Hash import SHA256
 import os
+import base64
 
 def generate_master_key(password:str):
     master_password = password.encode()
@@ -12,3 +13,15 @@ def generate_master_key(password:str):
                         hmac_hash_module=SHA256)
     
     return master_key
+
+def bytes_to_string(key: bytes) -> str: 
+    b64_encoded_key = base64.b64encode(key)
+    key_string = b64_encoded_key.decode()
+    
+    return key_string
+
+def string_to_bytes(key_string: str) -> bytes: 
+    b64_decoded_key = base64.b64decode(key_string)
+    
+    return b64_decoded_key
+
